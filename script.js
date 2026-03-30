71bfa6729c3da16476d2a78e811913f6262a5517
arpitgoyal1306 <arpitg1306@gmail.com>
2026-03-30 17:06:43 +0530

    style css

---
+++ script.js
@@ -0,0 +1,8 @@
+const recipeContainer = document.getElementById("recipeContainer");
+
+recipeContainer.innerHTML = `
+  <div class="card">
+    <img src="https://cdn.dummyjson.com/recipe-images/1.webp">
+    <h3>Test Recipe</h3>
+  </div>
+`;
