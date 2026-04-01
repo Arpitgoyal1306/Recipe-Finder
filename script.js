2ae6dedaddd3196f10c83ad5c6181aca9796535e
arpitgoyal1306 <arpitg1306@gmail.com>
2026-04-01 20:15:58 +0530

    filtering and sorting

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const searchButton = document.getElementById("searchButton");
 const searchInput = document.getElementById("searchInput");
 const recipeContainer = document.getElementById("recipeContainer");
 const loading = document.getElementById("loading");
+const mealTypeSelect = document.getElementById("mealTypeSelect");
 
 searchButton.addEventListener("click", getRecipes);
+mealTypeSelect.addEventListener("change", filterByMealType);
 
 async function getRecipes() {
   let query = searchInput.value;
@@ -19,41 +21,65 @@ async function getRecipes() {
   let response = await fetch("https://dummyjson.com/recipes/search?q=" + query);
 
   let data = await response.json();
-  
+
   loading.style.display = "none";
-  
-  data.recipes.forEach(function (recipe) {
-    let card = document.createElement("div");
-    
-    card.className = "card";
-    
-    card.innerHTML =
-    "<img src='" + recipe.image + "'>" + "<h3>" + recipe.name + "</h3>";
-    
-    recipeContainer.appendChild(card);
-  });
+
+  displayRecipes(data.recipes);
 }
 
 async function loadAllRecipes() {
   loading.style.display = "block";
   recipeContainer.innerHTML = "";
-  
-  let response = await fetch("https://dummyjson.com/recipes")
+
+  let response = await fetch("https://dummyjson.com/recipes");
+
   let data = await response.json();
 
   loading.style.display = "none";
 
-  data.recipes.forEach(function (recipe) {
-    let card = document.createElement("div");
+  displayRecipes(data.recipes);
+}
 
-    card.className = "card";
+async function filterByMealType() {
+  let selectedType = mealTypeSelect.value;
 
-    card.innerHTML =
-      "<img src='" + recipe.image + "'>" + "<h3>" + recipe.name + "</h3>";
+  loading.style.display = "block";
+  recipeContainer.innerHTML = "";
 
-    recipeContainer.appendChild(card)
+  let response = await fetch("https://dummyjson.com/recipes");
+
+  let data = await response.json();
+
+
+  loading.style.display = "none";
+
+  let recipes = data.recipes;
+
+  if (selectedType !== "All") {
+    recipes = recipes.filter(function(recipe) {
+
+      return recipe.mealType.includes(selectedType);
+
+});
+  }
+
+  displayRecipes(recipes);
+}
+
+function displayRecipes(recipes) {
+  recipeContainer.innerHTML = "";
+
+  recipes.forEach(function (recipe) {
+let card = document.createElement("div");
+
+card.className = "card";
+
+card.innerHTML =
+  "<img src='" + recipe.image + "'>" +
+  "<h3>" + recipe.name + "</h3>";
+
+recipeContainer.appendChild(card);
   });
 }
 
 loadAllRecipes();
-
